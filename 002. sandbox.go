8aeb0b620cb3a549020a66c4451dee7b956080c6
Mojammel Haque Bhuiyan <98250129+mhbhuiyan99@users.noreply.github.com>
2025-05-29 12:16:25 +0600

    Create 002. sandbox.go

---
+++ 002. sandbox.go
@@ -0,0 +1,17 @@
+Go: https://go.dev/tour/welcome/4
+
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	fmt.Println("Welcome to the playground!")
+
+	fmt.Println("The time is", time.Now())
+}
+---------
+Welcome to the playground!
+The time is 2025-05-29 12:16:11.419035 +0600 +06 m=+0.000519601
