bd2542e143f798bc86ed1081a4e29ffa469d5d1e
Mojammel Haque Bhuiyan <98250129+mhbhuiyan99@users.noreply.github.com>
2025-07-01 22:48:25 +0600

    Create 003. package.go

---
+++ 003. package.go
@@ -0,0 +1,11 @@
+Link : [package.go](https://go.dev/tour/basics/1)
+package main
+
+import (
+    "fmt"       // For formatted I/O (printing, scanning)
+    "math/rand" // For random number generation
+)
+
+func main() {
+	fmt.Println("My favorite number is", rand.Intn(10))
+}
